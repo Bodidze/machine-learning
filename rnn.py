ec04d2eb7cc63c4578dafd9b4d593e59a88379d6
Bodidze <bfedonyk@gmail.com>
2016-03-14 16:11:27 +0200

    expanding model

--- rnn.py
+++ rnn.py
@@ -1,9 +1,13 @@
+import theano
+theano.config.device = 'gpu'
+theano.config.floatX = 'float32'
 from keras.preprocessing import sequence
 from keras.utils import np_utils
 from keras.models import Sequential
 from keras.layers.core import Dense, Dropout, Activation
 from keras.layers.embeddings import Embedding
 from keras.layers.recurrent import LSTM
+from keras.callbacks import EarlyStopping
 
 max_features = 100000
 maxlen = 100
@@ -17,9 +21,17 @@ model.add(Dropout(0.5))
 model.add(Dense(1))
 model.add(Activation('sigmoid'))
 
+model = model_from_json(open('my_model_architecture.json').read())
+model.load_weights('my_model_weights.h5')
+
+early_stopping = EarlyStopping(monitor='val_loss', patience=2)
+model.fit(X, y, validation_split=0.2, callbacks=[early_stopping])
+
 model.compile(loss='binary_crossentropy',
               optimizer='adam',
               class_mode="binary")
+
+
 
 model.fit(
     X_train, y_train,
@@ -28,4 +40,12 @@ model.fit(
     show_accuracy=True
 )
 
+
+
+
 result = model.predict_proba(X)
+
+json_string = model.to_json()
+open('my_model_architecture.json', 'w').write(json_string)
+model.save_weights('my_model_weights.h5')
+
